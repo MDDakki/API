ad19c978a01eb6a55a4d9a1de0b6b71b754fc000
MoDakki <mohammaddakki@gmail.com>
2024-01-11 16:34:55 +0100

    Format

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,22 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function GetUsers() {
-    const Response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const Data = await Response.json()
-    const benutzerlist = document.querySelector('.benutzer-liste')
+  const Response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const Data = await Response.json();
+  const benutzerlist = document.querySelector(".benutzer-liste");
 
-    benutzerlist.innerHTML = Data.map(benutzer => `<div class="benutzer-karte">
+  benutzerlist.innerHTML = Data.map((benutzer) => BenutzerHTML(benutzer)).join("")
+}
+
+GetUsers();
+
+function BenutzerHTML(benutzer) {
+  return `<div class="benutzer-karte">
     <div class="benutzer-karte__container">
       <h3>${benutzer.name}</h4>
         <p><b>Email:</b> ${benutzer.email}</p>
         <p><b>Webseite:</b> <a href="https://${benutzer.website}" target="_blank">${benutzer.website}</a></p>
-        <p><b>Handy Nummer:</b> ${benutzer.phone}</p>
+        <p><b>Handy Nummer:</b> ${benutzer.phone}</sp>
     </div>
-  </div>`).join('')
+  </div>`;
 }
-
-GetUsers()
